c9afeec27218389901d366fac3bae0b43dd79679
HU JIAJUN <hujiajun19961024@gmail.com>
2022-01-20 19:34:51 +0800

    34, 37, 51, 69, 79, 128, 154, 215, 232, 257, 278, 347, 451, 503, 594, 739

--- Test/src/holmesjj/com/Main.java
+++ Test/src/holmesjj/com/Main.java
@@ -7,71 +7,103 @@ import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Queue;
 import java.util.Stack;
 import java.util.TreeMap;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main {
 
-    public static int[] kWeakestRows(int[][] mat, int k) {
-        TreeMap<Integer, List<Integer>> treeMap = new TreeMap<>();
-        for (int i = 0; i < mat.length; i++) {
-            // 每一行用二分搜索找到最后一个士兵的位置
-            int start = 0;
-            int end = mat[i].length - 1;
-            while (start < end) {
-                if (start == end - 1) {
-                    break;
-                }
-                int mid = start + (end - start) / 2;
-                if (mat[i][mid] == 1) {
-                    start = mid;
-                } else {
-                    end = mid;
-                }
-            }
-            List<Integer> indexes;
-            // start就是最后一个士兵的位置
-            if (treeMap.get(start) != null) {
-                indexes = treeMap.get(start);
-                indexes.add(i);
-            } else {
-                indexes = new ArrayList<>();
-                indexes.add(i);
-            }
-            Collections.sort(indexes);
-            treeMap.put(start, indexes);
+    public static List<List<String>> solveNQueens(int n) {
+
+        List<List<String>> results = new ArrayList<>();
+        List<String> result = new ArrayList<>();
+        boolean[][] isValid = new boolean[n][n];
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(isValid[i], true);
         }
-        int[] result = new int[k];
-        int m = 0;
-        for (Map.Entry<Integer, List<Integer>> entry : treeMap.entrySet()) {
-            if (m == k) {
-                break;
-            }
-            List<Integer> indexes = entry.getValue();
-            for (int i = 0; i < indexes.size(); i++) {
-                if (m == k) {
-                    break;
+        backtracking(results, result, isValid, 0, n);
+        return results;
+    }
+
+    public static void backtracking(List<List<String>> results, List<String> result, boolean[][] isValid, int queens, int n) {
+
+        // 皇后的数量
+        if (queens == n) {
+            // 收集结果，需要创建一个新的对象
+            results.add(new ArrayList<>(result));
+            return;
+        }
+
+        // 处理结点
+        // 不需要每次都从第一行扫描，因为每个皇后肯定只能存在一行中
+        // 有多少个皇后就代表前几行都不能再放皇后
+        // 只需要遍历当前没有皇后的行即可
+        int a = queens;
+        for (int b = 0; b < n; b++) {
+            if (isValid[a][b]) {
+                // 当前皇后的位置为a, b
+
+                // 复制当前可行区域
+                boolean[][] nIsValid = new boolean[n][n];
+                for (int i = 0; i < n; i++) {
+                    for (int j = 0; j < n; j++) {
+                        nIsValid[i][j] = isValid[i][j];
+                    }
                 }
-                result[m] = indexes.get(i);
-                m++;
+
+                // 摆放一个皇后，更新剩下可行的格子
+                // 00 01 02 03
+                // 10 11 12 13
+                // 20 21 22 23
+                // 30 31 32 33
+                for (int i = queens; i < n; i++) {
+                    for (int j = 0; j < n; j++) {
+                        // 行
+                        if (i == a) {
+                            nIsValid[i][j] = false;
+                        }
+                        // 列
+                        if (j == b) {
+                            nIsValid[i][j] = false;
+                        }
+                        // 斜线
+                        if (a - b == i - j) {
+                            nIsValid[i][j] = false;
+                        }
+                        if (a + b == i + j) {
+                            nIsValid[i][j] = false;
+                        }
+                    }
+                }
+
+                // 添加放置了当前皇后的行
+                String line = "";
+                for (int i = 0; i < n; i++) {
+                    if (i != a) {
+                        line = line + ".";
+                    } else {
+                        line = line + "Q";
+                    }
+                }
+                result.add(line);
+                queens++;
+                backtracking(results, result, nIsValid, queens, n);
+                // 回溯操作
+                result.remove(result.size() - 1);
+                queens--;
             }
         }
-        return result;
     }
 
     public static void main(String[] args) throws IOException {
-        int[][] mat = new int[5][5];
-        mat[0] = new int[]{1,1,0,0,0};
-        mat[1] = new int[]{1,1,1,1,0};
-        mat[2] = new int[]{1,0,0,0,0};
-        mat[3] = new int[]{1,1,0,0,0};
-        mat[4] = new int[]{1,1,1,1,1};
-        kWeakestRows(mat, 1);
+        solveNQueens(4);
     }
 }
