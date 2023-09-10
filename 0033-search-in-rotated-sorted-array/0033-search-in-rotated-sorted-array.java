f138e079780706770f52f934bd6bf50570387489
Hu JiaJun <62185825+HolmesJJ@users.noreply.github.com>
2023-09-10 23:23:31 +0800

    Time: 0 ms (100.00%), Space: 40.8 MB (53.77%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,22 +1,45 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int pos = binarySearch(nums, target, 0, nums.length - 1);
-        if (pos == nums.length || nums[pos] != target) {
-            return -1;
-        }
+        int pos = binarySearch(nums, target);
         return pos;
     }
-    
-    public int binarySearch(int[] nums, int target, int low, int high) {
-        if (low > high) {
-            return low;
+
+    public int binarySearch(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if ((nums[mid] > nums[nums.length - 1] && (nums[mid] < target || target <= nums[nums.length - 1])) ||
+                       (nums[mid] <= nums[nums.length - 1]) && target <= nums[nums.length - 1] && nums[mid] < target) {
+                start = mid;
+            } else {
+                end = mid;
+            }
         }
-        int mid = low + (high - low) / 2;
-        if ((nums[mid] > nums[nums.length - 1] && (target > nums[mid] || target <= nums[nums.length - 1])) ||
-            (nums[mid] <= nums[nums.length - 1] && target <= nums[nums.length - 1] && nums[mid] < target)) {
-            return binarySearch(nums, target, mid + 1, high);
+        if (nums[start] == target) {
+            return start;
+        } else if (nums[end] == target) {
+            return end;
         } else {
-            return binarySearch(nums, target, low, mid - 1);
+            return -1;
         }
     }
 }
+
+
+
+
+    // public int binarySearch(int[] nums, int target, int low, int high) {
+    //     if (low > high) {
+    //         return low;
+    //     }
+    //     int mid = low + (high - low) / 2;
+    //     if ((nums[mid] > nums[nums.length - 1] && (target > nums[mid] || target <= nums[nums.length - 1])) ||
+    //         (nums[mid] <= nums[nums.length - 1] && target <= nums[nums.length - 1] && nums[mid] < target)) {
+    //         return binarySearch(nums, target, mid + 1, high);
+    //     } else {
+    //         return binarySearch(nums, target, low, mid - 1);
+    //     }
+    // }
