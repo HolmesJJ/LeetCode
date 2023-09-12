3c234a7d9a036e3323b8d8d1efe83a67a89a067f
Hu JiaJun <62185825+HolmesJJ@users.noreply.github.com>
2023-09-12 16:33:53 +0800

    Time: 2 ms (79.57%), Space: 60.7 MB (83.04%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,16 +1,11 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        if (prices.length <= 1) {
-            return 0;
-        }
-        int min = prices[0];
-        int result = 0;
-        for (int i = 1; i < prices.length; i++) {
-            if (prices[i] - min > result) {
-                result = prices[i] - min;
-            }
+        int min = Integer.MAX_VALUE;
+        int max = 0;
+        for (int i = 0; i < prices.length; i++) {
             min = Math.min(min, prices[i]);
+            max = Math.max(max, prices[i] - min);
         }
-        return result;
+        return max;
     }
 }
