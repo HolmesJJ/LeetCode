2601ae5a5f3b5c5ca7ddcafed1087561aacdb34e
Hu JiaJun <62185825+HolmesJJ@users.noreply.github.com>
2023-10-29 17:45:33 +0800

    Time: 2267 ms (5.01%), Space: 44.3 MB (5.05%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,53 +1,38 @@
 class Solution {
+    boolean isWord = false;
+    
     public boolean exist(char[][] board, String word) {
         int m = board.length;
         int n = board[0].length;
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[0].length; j++) {
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
                 if (word.charAt(0) == board[i][j]) {
                     boolean[][] visited = new boolean[m][n];
                     visited[i][j] = true;
-                    boolean isExist = backtracking(board, m, n, word, word.length(), visited, i, j, 0);
-                    if (isExist) {
-                        return true;
-                    }
+                    backtracking(board, visited, i, j, board[i][j] + "", word);
                 }
             }
         }
-        return false;
+        return isWord;
     }
     
-    // i和j：记录当前字母在board的位置
-    // li和lj：记录上一个字母在board的位置
-    public boolean backtracking(char[][] board, int m, int n, String word, int len, boolean[][] visited, int i, int j, int pos) {
-        boolean isExist = false;
-        if (pos == len - 1) {
-            isExist = true;
-            return isExist;
+    public void backtracking(char[][] board, boolean[][] visited, int x, int y, String result, String word) {
+        if (result.equals(word)) {
+            isWord = true;
+            return;
         }
-        int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-        
-        // 遍历集合元素：四个方向
-        for (int dir = 0; dir < dirs.length; dir++) {
-            // 下一个字母的位置
-            int ni = i + dirs[dir][0];
-            int nj = j + dirs[dir][1];
-            // 处理结点
-            if (ni >= 0 && ni < m && nj >= 0 && nj < n && visited[ni][nj] == false) {
-                char nextChar = word.charAt(pos + 1);
-                if (nextChar == board[ni][nj]) {
-                    visited[ni][nj] = true;
-                    pos++;
-                    isExist = backtracking(board, m, n, word, len, visited, ni, nj, pos);
-                    if (isExist) {
-                        break;
-                    }
-                    // 回溯操作
-                    pos--;
-                    visited[ni][nj] = false;
-                }
+        if (result.length() > word.length()) {
+            return;
+        }
+        int[][] dirs = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+        for (int[] dir : dirs) {
+            int newX = x + dir[0];
+            int newY = y + dir[1];
+            if (newX >= 0 && newX < board.length && newY >= 0 && newY < board[0].length && !visited[newX][newY]) {
+                visited[newX][newY] = true;
+                backtracking(board, visited, newX, newY, result + board[newX][newY], word);
+                visited[newX][newY] = false;
             }
         }
-        return isExist;
     }
 }
