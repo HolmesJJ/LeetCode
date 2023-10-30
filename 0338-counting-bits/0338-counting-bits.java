be9b09ffc14af4ab6296d1e03994e2d73bc865e6
Hu JiaJun <62185825+HolmesJJ@users.noreply.github.com>
2023-10-30 11:32:47 +0800

    Time: 2 ms (84.04%), Space: 46.5 MB (43.56%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -2,16 +2,25 @@ class Solution {
     public int[] countBits(int n) {
         int[] f = new int[n + 1];
         f[0] = 0;
+        int i = 1;
+        int j = 0;
         int max = 1;
-        int pointer = 0;
-        for (int i = 1; i <= n; i++) {
+        while (i <= n) {
             if (i == max) {
                 max = max * 2;
-                pointer = 0;
+                j = 0;
             }
-            f[i] = f[pointer] + 1;
-            pointer++;
+            f[i] = f[j] + 1;
+            i++;
+            j++;
         }
         return f;
     }
 }
+
+// 0 00   0
+// 1 01   1
+// 2 10   1
+// 3 11   2
+// 4 100  3
+// 5 101  3
