32cf4dca3e3bda3ff11a432b90be34a5a1cdeb6c
Hu JiaJun <62185825+HolmesJJ@users.noreply.github.com>
2023-11-07 17:01:38 +0800

    Time: 45 ms (6.01%), Space: 64.7 MB (5.16%) - LeetHub

---
+++ 0456-132-pattern/0456-132-pattern.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean find132pattern(int[] nums) {
+        if (nums == null || nums.length < 3) {
+            return false;
+        }
+        int min = nums[0];
+        Stack<int[]> stack = new Stack<>();
+        for (int i = 1; i < nums.length; i++) {
+            while (!stack.isEmpty()) {
+                int[] top = stack.peek();
+                if (nums[i] >= top[0]) {
+                    stack.pop();
+                } else {
+                    break;
+                }
+            }
+            if (!stack.isEmpty()) {
+                int[] top = stack.peek();
+                if (nums[i] > top[1]) {
+                    return true;
+                }
+            }
+            stack.push(new int[]{nums[i], min});
+            min = Math.min(min, nums[i]);
+        }
+        return false;
+    }
+}
