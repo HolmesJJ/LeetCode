27ce8501dfab70c5fec098a5217b1c52390d06fa
Hu JiaJun <62185825+HolmesJJ@users.noreply.github.com>
2023-11-07 17:02:49 +0800

    Time: 50 ms (5.04%), Space: 65 MB (5.16%) - LeetHub

--- 0456-132-pattern/0456-132-pattern.java
+++ 0456-132-pattern/0456-132-pattern.java
@@ -5,6 +5,7 @@ class Solution {
         }
         int min = nums[0];
         Stack<int[]> stack = new Stack<>();
+        stack.push(new int[]{nums[0], min});
         for (int i = 1; i < nums.length; i++) {
             while (!stack.isEmpty()) {
                 int[] top = stack.peek();
