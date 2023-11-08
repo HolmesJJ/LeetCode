4d3eb194d972e22ff72f27b73af51866ba046fc6
Hu JiaJun <62185825+HolmesJJ@users.noreply.github.com>
2023-11-08 20:49:02 +0800

    Time: 1164 ms (10.11%), Space: 60.6 MB (5.09%) - LeetHub

---
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -0,0 +1,42 @@
+class Solution {
+    public void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    public int partition(int[] nums, int low, int high) {
+        int pivot = nums[high];
+        int left = low;
+        int right = high - 1;
+        while (true) {
+            while (left <= right && nums[left] < pivot) {
+                left++;
+            }
+            while (left <= right && nums[right] > pivot) {
+                right--;
+            }
+            if (left >= right) {
+                break;
+            }
+            swap(nums, left, right);
+            left++;
+            right--;
+        }
+        swap(nums, left, high);
+        return left;
+    }
+
+    public void sort(int[] nums, int low, int high) {
+        if (low < high) {
+            int pi = partition(nums, low, high);
+            sort(nums, low, pi - 1);
+            sort(nums, pi + 1, high);
+        }
+    }
+
+    public int[] sortArray(int[] nums) {
+        sort(nums, 0, nums.length - 1);
+        return nums;
+    }
+}
