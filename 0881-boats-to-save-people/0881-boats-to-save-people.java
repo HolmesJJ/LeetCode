94bd7909eb3fac8a1504c62beb7581e9a82581a8
Hu JiaJun <62185825+HolmesJJ@users.noreply.github.com>
2023-11-08 11:14:33 +0800

    Time: 16 ms (95.74%), Space: 53.9 MB (40.29%) - LeetHub

---
+++ 0881-boats-to-save-people/0881-boats-to-save-people.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int res = 0;
+        int l = 0;
+        int r = people.length - 1;
+        while (l <= r) {
+            if (people[r] + people[l] <= limit) {
+                l++;
+            }
+            r--;
+            res++;
+        }
+        return res;
+    }
+}
