66941432030c02506d9baa76850be0d8f33e2129
Hu JiaJun <62185825+HolmesJJ@users.noreply.github.com>
2023-12-07 15:27:47 +0800

    Time: 84 ms (51.17%), Space: 100.4 MB (13.22%) - LeetHub

---
+++ 0463-island-perimeter/0463-island-perimeter.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    int islandPerimeter(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        
+        int res = 0;
+        
+        bool visited[m][n];
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                visited[i][j] = false;
+            }
+        }
+        
+        int dirs[4][2] = {{0, -1}, {0, 1}, {-1, 0}, {1, 0}};
+        std::queue<std::pair<int, int>> q;
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    q.push({i, j});
+                    visited[i][j] = true;
+                    break;
+                }
+            }
+        }
+        
+        while (!q.empty()) {
+            std::pair<int, int> node = q.front();
+            q.pop();
+            int x = node.first;
+            int y = node.second;
+            res = res + 4;
+            for (auto& dir : dirs) {
+                int newX = x + dir[0];
+                int newY = y + dir[1];
+                if (newX >= 0 && newX < m && newY >= 0 && newY < n) {
+                    if (grid[newX][newY] == 1) {
+                        res--;
+                        if (!visited[newX][newY]) {
+                            q.push({newX, newY});
+                            visited[newX][newY] = true;
+                        }
+                    }
+                }
+            }
+        }
+        
+        return res;
+    }
+};
