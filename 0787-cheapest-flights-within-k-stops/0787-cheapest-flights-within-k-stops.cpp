4e25bbd5c13e238184555d9884c1657bc7b1d418
Hu JiaJun <62185825+HolmesJJ@users.noreply.github.com>
2024-01-03 13:05:36 +0800

    Time: 27 ms (31.42%), Space: 13.5 MB (77.52%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,23 +1,29 @@
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        vector<vector<pair<int, int>>> graph(n);
+        for (auto& flight : flights) {
+            graph[flight[0]].push_back({flight[1], flight[2]});
+        }
+        
         vector<int> prices(n, INT_MAX);
         prices[src] = 0;
 
-        for (int i = 0; i < k + 1; ++i) {
+        // Bellman-Ford algorithm
+        for (int i = 0; i <= k; i++) {
             vector<int> tmpPrices(prices);
-            for (const auto& flight : flights) {
-                int s = flight[0], d = flight[1], p = flight[2];
-                if (prices[s] == INT_MAX) {
-                    continue;
-                }
-                if (prices[s] + p < tmpPrices[d]) {
-                    tmpPrices[d] = prices[s] + p;
+            for (int u = 0; u < n; u++) {
+                for (auto& v : graph[u]) {
+                    int next = v.first;
+                    int price = v.second;
+                    if (prices[u] != INT_MAX && prices[u] + price < tmpPrices[next]) {
+                        tmpPrices[next] = prices[u] + price;
+                    }
                 }
             }
-            prices = tmpPrices;
+            prices.swap(tmpPrices);
         }
-        
+
         return prices[dst] == INT_MAX ? -1 : prices[dst];
     }
 };
