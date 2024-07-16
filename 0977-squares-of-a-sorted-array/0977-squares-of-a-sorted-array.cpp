6d40347d52b6b9960af818a08343dbf5e4d0ee6e
Hu JiaJun <62185825+HolmesJJ@users.noreply.github.com>
2024-07-16 17:22:07 +0800

    Time: 20 ms (73.05%), Space: 29.6 MB (26.98%) - LeetHub

---
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        int low = 0;
+        int high = nums.size() - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] < 0) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        cout << low << endl;
+        vector<int> results;
+        int left = low - 1;
+        int right = low;
+        while (left >= 0 && right < nums.size()) {
+            int l = nums[left] * nums[left];
+            int r = nums[right] * nums[right];
+            if (l < r) {
+                results.push_back(l);
+                left--;
+            } else {
+                results.push_back(r);
+                right++;
+            }
+        }
+        while (left >= 0) {
+            int l = nums[left] * nums[left];
+            results.push_back(l);
+            left--;
+        }
+        while (right < nums.size()) {
+            int r = nums[right] * nums[right];
+            results.push_back(r);
+            right++;
+        }
+        return results;
+    }
+};
